615a78db21d1de4cf9fcfdc6e3e8be22a5cfcf11
Dor Shir <Dorshir92@gmail.com>
2024-01-23 20:02:45 +0200

    whitespacee

--- Assets/Scripts/mover.cs
+++ Assets/Scripts/mover.cs
@@ -9,11 +9,10 @@ public class Mover : MonoBehaviour
     public float amplitude = 1.0f;  // גובה התנועה
     public float frequency = 1.0f;  // תדר התנועה
 
-
     // Start is called before the first frame update
     void Start()
     {
-        
+
         prevScale = GetComponent<Transform>().localScale;
         transform.position = new Vector3(-4, 0, 0);
     }
@@ -21,23 +20,24 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         if(Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-           state = !state;
-        
-           if( state ) {
-               GetComponent<Transform>().localScale = new Vector3(0,0,0); //transform.localScale = new Vector3(0,0,0);
-           }
-          else {
-              GetComponent<Transform>().localScale = prevScale;
-          }
+            state = !state;
+
+            if (state)
+            {
+                GetComponent<Transform>().localScale = new Vector3(0, 0, 0); //transform.localScale = new Vector3(0,0,0);
+            }
+            else
+            {
+                GetComponent<Transform>().localScale = prevScale;
+            }
         }
-        float newX = transform.position.x/4 + Mathf.Sin(frequency*Time.time) * Time.deltaTime ;
-        float newY = 1-Mathf.Sqrt(2-newX*newX);
+        float newX = transform.position.x / 4 + Mathf.Sin(frequency * Time.time) * Time.deltaTime;
+        float newY = 1 - Mathf.Sqrt(2 - newX * newX);
         // Console.WriteLine(newX, newY);
         // Debug.Log("newX=" + newX + " newY=" + newY + " freq=" + frequency + " sin=" + Mathf.Sin(frequency*Time.time));
         // Debug.Log(newY);
-        transform.position = new Vector3(4*newX, 4*newY, transform.position.z);
+        transform.position = new Vector3(4 * newX, 4 * newY, transform.position.z);
     }
 }
-    
