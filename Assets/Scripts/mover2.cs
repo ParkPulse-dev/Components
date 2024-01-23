bd3811a75603c02b5feaf1a51341fca4cd298799
AvivShevach <92975487+AvivShevach@users.noreply.github.com>
2024-01-23 19:52:16 +0200

    Update mover2.cs

--- Assets/Scripts/mover2.cs
+++ Assets/Scripts/mover2.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class mover2 : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speedRotation;
     Vector3 prevScale;
     void Start()
     {
         prevScale = GetComponent<Transform>().localScale;
-        speedRotation = 50f;
     }
 
     bool state = false;
@@ -28,6 +26,6 @@ public class mover2 : MonoBehaviour
               GetComponent<Transform>().localScale = prevScale;
           }
         }
-        transform.rotation = Quaternion.Euler(0, 0, (Time.time * speedRotation) % 360);
+        transform.rotation = Quaternion.Euler(0, 0, (Time.time * 50) % 360);
     }
 }
