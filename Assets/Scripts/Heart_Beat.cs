66dd0599a89f7bca19fc2c53ec60058a71101e3b
Dor Shir <Dorshir92@gmail.com>
2024-01-23 20:17:23 +0200

    heart

--- Assets/Scripts/Heart_Beat.cs
+++ Assets/Scripts/Heart_Beat.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Heart_Beat : MonoBehaviour
 {
     Vector3 prevScale;
@@ -21,45 +20,58 @@ public class Heart_Beat : MonoBehaviour
     bool state = false;
     void Update()
     {
-        // Calculate the scaling factor based on time and speed
-         if(Input.GetKeyDown(KeyCode.Alpha3))
-        {
-           state = !state;
-        
-           if( state ) {
-               GetComponent<Transform>().localScale = new Vector3(0,0,0); //transform.localScale = new Vector3(0,0,0);
-           }
-          else {
-              GetComponent<Transform>().localScale = prevScale;
-          }
-        }
         float scaleChange = pulseSpeed * Time.deltaTime;
+        float newGrowX = initialScale.x * scaleChange;
+        float newGrowY = initialScale.y * scaleChange;
+        float newGrowZ = initialScale.z * scaleChange;
+        float minX = initialScale.x * maxScaleFactor;
+        float minY = initialScale.y * maxScaleFactor;
+        float minY = initialScale.z * maxScaleFactor;
+        float maxX = initialScale.x * maxScaleFactor;
+        float maxY = initialScale.y * maxScaleFactor;
+        float maxZ = initialScale.z * maxScaleFactor;
 
+        // Calculate the scaling factor based on time and speed
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = !state;
+
+            if (state)
+            {
+                GetComponent<Transform>().localScale = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                GetComponent<Transform>().localScale = prevScale;
+            }
+        }
         // Determine whether to grow or shrink
         if (isGrowing)
         {
-            transform.localScale += new Vector3(initialScale.x * scaleChange, initialScale.y * scaleChange, initialScale.z * scaleChange);
+            transform.localScale += new Vector3(newGrowX, newGrowY, newGrowZ);
 
             // Switch to shrinking if reaching the maximum scale
-            if (transform.localScale.x >= initialScale.x * maxScaleFactor)
+            if (transform.localScale.x >= maxX)
             {
-                transform.localScale = new Vector3(initialScale.x * maxScaleFactor, initialScale.y * maxScaleFactor, initialScale.z * maxScaleFactor);
+                float newShrinkX = initialScale.x * maxScaleFactor;
+                float newShrinkY = initialScale.y * maxScaleFactor;
+                float newShrinkZ = initialScale.z * maxScaleFactor;
+                transform.localScale = new Vector3(maxX, maxY, maxZ);
                 isGrowing = false;
             }
         }
         else
         {
-            transform.localScale -= new Vector3(initialScale.x * scaleChange, initialScale.y * scaleChange, initialScale.z * scaleChange);
+            transform.localScale -= new Vector3(newGrowX, newGrowY, newGrowZ);
 
             // Switch to growing if reaching the minimum scale
-            if (transform.localScale.x <= initialScale.x * minScaleFactor)
+            if (transform.localScale.x <= minX)
             {
-                transform.localScale = new Vector3(initialScale.x * minScaleFactor, initialScale.y * minScaleFactor, initialScale.z * minScaleFactor);
+                transform.localScale = new Vector3(minX, minY, minZ);
                 isGrowing = true;
             }
         }
-
         // Ensure the object stays in the same Y position
         transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
-    }
+    }
 }
